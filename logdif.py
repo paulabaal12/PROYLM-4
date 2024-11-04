e37e8a4deaa5cd57d178be23699b952f601b8265
Hayser8 <105123207+Hayser8@users.noreply.github.com>
2024-11-03 20:04:47 -0600

    Funciones de membresía temp

--- logdif.py
+++ logdif.py
@@ -8,3 +8,8 @@ ocupacion = ctrl.Antecedent(np.arange(0, 11, 1), 'ocupacion')
 
 # Variable de salida
 enfriamiento = ctrl.Consequent(np.arange(0, 101, 1), 'enfriamiento')
+
+# Funciones de membresía para temperatura
+temperatura['baja'] = fuzz.trimf(temperatura.universe, [15, 15, 22])
+temperatura['media'] = fuzz.trimf(temperatura.universe, [20, 25, 28])
+temperatura['alta'] = fuzz.trimf(temperatura.universe, [26, 35, 35])
